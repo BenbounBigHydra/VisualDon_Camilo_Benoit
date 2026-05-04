f9557e2266ca99c5194b7734081ee53de97c7e0c
BenbounBigHydra <benoit.jaques@sunrise.ch>
2026-05-04 13:54:02 +0200

    style for home and person question

--- src/pages/blindtest-page.js
+++ src/pages/blindtest-page.js
@@ -33,9 +33,11 @@ customElements.define('blindtest-page', class extends HTMLElement {
         this.loadTitles();
         const title = await this.getRandomTitle();
         this.innerHTML = `
-            <div id="embed-iframe"></div>
-            <div id="listener"></div>
-            <blindtest-question title-id="${title.id}"></blindtest-question>
+            <div class="border border-primary rounded-3 p-4 shadow-sm bg-light mx-auto" style="width: 90%;">
+                <div id="embed-iframe"></div>
+                <div id="listener"></div>
+                <blindtest-question title-id="${title.id}"></blindtest-question>
+            </div>
         `
         this.addEventListener('play_click', () => alert('1'))
     }
@@ -44,9 +46,12 @@ customElements.define('blindtest-page', class extends HTMLElement {
         const answered = getAnswered();
 
         this.innerHTML = `
-            <h2></h2>
-            <word-cloud></word-cloud>
-            <img id="scroll-arrow" src="./src/assets/Asset 1.svg" alt="flèche vers le bas">
+            <div class="border border-primary rounded-3 p-4 shadow-sm bg-light mx-auto d-flex justify-content-center flex-column align-items-center" style="width: 90%;">
+                <h2></h2>
+                <word-cloud></word-cloud>
+                <img class="pt-3" style="max-height: 15vh;" id="scroll-arrow" src="./src/assets/Asset 1.svg" alt="flèche vers le bas">
+            </div>
+
         `;
         const title = this.querySelector('h2');
         const wordCloud = this.querySelector('word-cloud');
@@ -65,7 +70,7 @@ customElements.define('blindtest-page', class extends HTMLElement {
 
         } else if (answered.includes('current-genres')) {
 
-            title.innerText = "question education musicale?";
+            title.innerText = "Quelle éducation musicale as-tu reçu ?";
             wordCloud.setAttribute('get-endpoint', 'education-levels');
             wordCloud.setAttribute('post-endpoint', 'education-levels');
 
--- src/pages/intro-page.js
+++ src/pages/intro-page.js
@@ -13,24 +13,32 @@ customElements.define('intro-page', class extends HTMLElement {
     }
 
     async render() {
+        console.dir(this);
+
         this.innerHTML = `
-            <h1>Nom du Projet</h1>
-            <p>jsp jsp il faudra qu'on décide d'une intro - définition de musique classique ou savante - but du projet</p>
+            <div class="border border-primary rounded-3 p-4 shadow-sm bg-light mx-auto" style="width: 90%;">
+                <h1>Classical Education</h1>
+                <p>Le but de cette représentation est d'explorer l'impact de la musique savante, communément appelée musique classique, sur la société actuelle à travers le degré de connaissance de différentes oeuvres et compositeurs.trices classiques. Nous voulons comprendre la corrélation possible entre l'éducation musicale d'une personne ainsi que ses préférences musicales et le degré de connaissance de cette personne vis-à-vis de la musique savante.</p>
+                <p style="font-style: italic;">Le terme "musique classique" sera régulièrement utilisé dans ce projet pour désigner la "musique savante".</p>
+            </div>
         `
 
         let userUuid = getUserUuid();
         if (! userUuid) {
             this.setUserUuid();
-            this.innerHTML += `
-                <p>Scrollez pour commencer !</p>
-                <img id="scroll-arrow" src="./src/assets/Asset 1.svg" alt="flèche vers le bas">
+            this.firstElementChild.innerHTML += `
+                <div class="d-flex gap-5 flex-column align-items-center">
+                    <img class="pt-3" style="max-height: 15vh;" id="scroll-arrow" src="./src/assets/Asset 1.svg" alt="flèche vers le bas">
+                    <p>Scrollez pour commencer !</p>
+                </div>
             `
         } else {
             const canAccessResults = JSON.parse(localStorage.getItem('can_access_results')) === 'true';
-            this.innerHTML += `
-                <div class="flex">
-                    <a class="btn border-2 rounded-0 btn-intro" href="../../blindtest.html">Blind Test</a>
-                    <a class="btn border-2 rounded-0 btn-intro ${canAccessResults ? '' : 'disabled'}" ${canAccessResults? 'href="../../results.html"' : ''}>Résultats</a>
+            this.firstElementChild.innerHTML += `
+                <div class="d-flex justify-content-center gap-5">
+                    <a class="btn border-2 rounded-2 btn-intro" href="../../blindtest.html">Blind Test</a>
+                    <a class="btn border-2 rounded-2 btn-intro ${canAccessResults ? '' : 'disabled'}" ${canAccessResults? 'href="../../results.html"' : ''}>Résultats</a>
+                </div>
             `
         }
         
