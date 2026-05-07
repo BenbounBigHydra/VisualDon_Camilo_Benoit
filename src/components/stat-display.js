bca659af0066e6dbc02210da62d0cdc000cf2062
Camilo <paula@pena-parra.ch>
2026-05-07 10:32:20 +0200

    affichage des stats d'un titre

--- src/components/stat-display.js
+++ src/components/stat-display.js
@@ -1,7 +1,7 @@
 import { getStats, getUser } from "../api";
 
 customElements.define("stat-display", class extends HTMLElement {
-    static observedAttributes = ['filter']
+    static observedAttributes = ['filter', 'labels']
     // filter : self (stats perso), all, self-taught, conservatory, hem, (vue générale), "title-id" (stats oeuvre)
 
     async connectedCallback() {
@@ -12,22 +12,18 @@ customElements.define("stat-display", class extends HTMLElement {
         await this.render();
     }
 
-    async loadTitleStats() {
-        const user = await getUser();
-        const stats = await getStats('titles', '552');
+    createWaffle(max, stats, highlight) {
         const total = [stats['unknown'], stats['known'], stats['bt-false'], stats['bt-composer'],stats['bt-title'],stats['bt-both']].reduce(((a, b) => a + b), 0);
         
-        console.log(stats, stats['unknown'], stats['known'], stats['bt-false'], stats['bt-composer'],stats['bt-title'],stats['bt-both'], total);
-
-        const unknown = total > 50? Math.round(stats['unknown']*60/total) : stats['unknown'];
-        const known = total > 50? Math.round(stats['known']*60/total) : stats['known'];
-        const btFalse = total > 50? Math.round(stats['bt-false']*60/total) : stats['bt-false'];
-        const btComposer = total > 50? Math.round(stats['bt-composer']*60/total) : stats['bt-composer'];
-        const btTitle = total > 50? Math.round(stats['bt-title']*60/total) : stats['bt-title'];
-        const btBoth = total > 50? Math.round(stats['bt-both']*60/total) : stats['bt-both'];
+        const unknown = total > max? Math.round(stats['unknown']*max/total) : stats['unknown'];
+        const known = total > max? Math.round(stats['known']*max/total) : stats['known'];
+        const btFalse = total > max? Math.round(stats['bt-false']*max/total) : stats['bt-false'];
+        const btComposer = total > max? Math.round(stats['bt-composer']*max/total) : stats['bt-composer'];
+        const btTitle = total > max? Math.round(stats['bt-title']*max/total) : stats['bt-title'];
+        const btBoth = total > max? Math.round(stats['bt-both']*max/total) : stats['bt-both'];
 
         const box = document.createElement('div');
-        box.setAttribute('class', 'd-flex flex-wrap-reverse stat-box gap-1 p-2 bg-white rounded');
+        box.setAttribute('class', 'd-flex flex-wrap-reverse align-content-start stat-box');
 
         for (let index = 0; index < unknown; index++) {
             box.innerHTML += '<div class="cell unknown"></div>';
@@ -47,32 +43,90 @@ customElements.define("stat-display", class extends HTMLElement {
         for (let index = 0; index < btBoth; index++) {
             box.innerHTML += '<div class="cell bt-both"></div>';
         }
-        
-        // Pour tester l'affichage
-        // for (let index = 0; index < 13*50/97; index++) {
-        //     box.innerHTML += '<div class="cell unknown"></div>';
-        // }
-        // for (let index = 0; index < 25*50/97; index++) {
-        //     box.innerHTML += '<div class="cell known"></div>';
-        // }
-        // for (let index = 0; index < 12*50/97; index++) {
-        //     box.innerHTML += '<div class="cell bt-false"></div>';
-        // }
-        // for (let index = 0; index < 4*50/97; index++) {
-        //     box.innerHTML += '<div class="cell bt-composer"></div>';
-        // }
-        // for (let index = 0; index < 26*50/97; index++) {
-        //     box.innerHTML += '<div class="cell bt-title"></div>';
-        // }
-        // for (let index = 0; index < 17*50/97; index++) {
-        //     box.innerHTML += '<div class="cell bt-both"></div>';
-        // }
 
+        if (highlight) {
+            box.querySelectorAll(`.${highlight}`).forEach(el => {
+                el.classList.add('highlight');
+            });
+        }
+
+        return box;
+    }
+
+    createLabels(stats) {
+        const total = [stats['unknown'], stats['known'], stats['bt-false'], stats['bt-composer'],stats['bt-title'],stats['bt-both']].reduce(((a, b) => a + b), 0);
+
+        const labels = document.createElement('div');
+        labels.setAttribute('class', 'd-flex flex-column gap-2');
+        labels.innerHTML = `
+            <div class="d-flex align-items-center gap-2">
+                <div class="cell bt-both"></div>
+                <p class="m-0">Deviné le compositeur et le titre : ${stats['bt-both']} personnes (${stats['bt-both']*100/total}%)</p>
+            </div>
+
+            <div class="d-flex align-items-center gap-2">
+                <div class="cell bt-title"></div>
+                <p class="m-0">Deviné le titre : ${stats['bt-title']} personnes (${stats['bt-title']*100/total}%)</p>
+            </div>
+
+            <div class="d-flex align-items-center gap-2">
+                <div class="cell bt-composer"></div>
+                <p class="m-0">Deviné le compositeur : ${stats['bt-composer']} personnes (${stats['bt-composer']*100/total}%)</p>
+            </div>
+
+            <div class="d-flex align-items-center gap-2">
+                <div class="cell bt-false"></div>
+                <p class="m-0">Deviné ni le compositeur ni le titre : ${stats['bt-false']} personnes (${stats['bt-false']*100/total}%)</p>
+            </div>
+
+            <div class="d-flex align-items-center gap-2">
+                <div class="cell known"></div>
+                <p class="m-0">Déjà entendu : ${stats['known']} personnes (${stats['known']*100/total}%)</p>
+            </div>
+
+            <div class="d-flex align-items-center gap-2">
+                <div class="cell unknown"></div>
+                <p class="m-0">Jamais entendu : ${stats['unknown']} personnes (${stats['unknown']*100/total}%)</p>
+            </div>
+        `
+
+        return labels;
+    }
+
+    async loadTitleStats(id) {
+        const user = await getUser();
+        const stats = await getStats('titles', id);
+
+        // Pour tester l'affichage
+        // const test = {
+        //     'unknown': 13,
+        //     'known': 29,
+        //     'bt-false': 12,
+        //     'bt-composer': 8,
+        //     'bt-title': 38,
+        //     'bt-both': 23,
+        // };
+        // const waffle = this.createWaffle(120, test, 'bt-composer');
+        
+        const waffle = this.createWaffle(120, stats);
+        const labels = this.createLabels(stats);
+
+        const box = document.createElement('div');
+        box.setAttribute('class', 'd-flex gap-2 p-1 align-items-center justify-content-end')
+        box.append(waffle);
+        if (this.getAttribute('labels') === 'true') {
+            box.append(labels);
+        }
+        this.innerHTML = '';
         this.append(box);
     }
 
     async render() {
-        this.loadTitleStats();
+
+        if (this.getAttribute('filter').startsWith('title-')) {
+            const id = this.getAttribute('filter').slice(6);
+            this.loadTitleStats(id);
+        }
     }
 
 })
